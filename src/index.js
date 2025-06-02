f68dd7541821f180f473972c0d96b6528bbe7c7e
clintjeff2 <clintjeff2@gmail.com>
2025-06-02 18:51:01 +0100

    feat: integrate challenge routes and middleware in main app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
-
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-  res.send('ChainVerse Certificate Generator API is running');
+	res.send('ChainVerse Certificate Generator API is running');
 });
+
+app.use('/api/challenges', require('./routes/challengeRoutes'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
